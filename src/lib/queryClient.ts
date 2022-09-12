abe68503efdfbf22256b597155791550ffc3b5d8
Nate Dunn <hello@natedunn.net>
2022-09-12 00:29:07 -0500

    Add client just for SSG

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,5 +1,16 @@
-import { createClient } from 'urql';
+import { createClient, cacheExchange, fetchExchange } from 'urql';
+import generatedSchema from '@codegen';
 
 export const client = createClient({
   url: `${process.env.BACKEND_URL}/graphql`,
 });
+
+// eslint-disable-next-line
+const schema = generatedSchema as any;
+cacheExchange(schema);
+
+export const ssgClient = () =>
+  createClient({
+    url: `${process.env.BACKEND_URL}/graphql`,
+    exchanges: [fetchExchange],
+  });
