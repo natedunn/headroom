68fb3b2cdd6a2b0cd724ebf4f46fa05e5e2ebec9
Nate Dunn <hello@natedunn.net>
2022-11-02 20:05:20 -0600

    Nuke Next.js

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { api } from '../server/api';
-
-export default async function Page() {
-  const test = await api.test();
-
-  return <div className='text-red-500'>Server Rendered Page: {test?.test}</div>;
-}
