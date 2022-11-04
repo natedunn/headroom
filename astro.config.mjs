882e7472a352116309805e1d85a99e4a2dec18c5
Nate Dunn <hello@natedunn.net>
2022-11-04 15:12:15 -0600

    Clean up imports for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import partytown from '@astrojs/partytown';
-import image from '@astrojs/image';
-import mdx from '@astrojs/mdx';
-import prefetch from '@astrojs/prefetch';
-import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify/edge-functions';
 
-// https://astro.build/config
-import netlify from '@astrojs/netlify/functions';
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://headroom.dev',
-  integrations: [tailwind(), partytown(), image(), mdx(), prefetch(), sitemap()],
+  integrations: [tailwind()],
   output: 'server',
-  adapter: netlify({
-    dist: new URL('./dist/', import.meta.url),
-  }),
+  adapter: netlify(),
 });
