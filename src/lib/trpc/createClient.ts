1b3442665215c6ada4176a734afadb0748eb1a56
Nate Dunn <hello@natedunn.net>
2022-11-03 19:25:44 -0600

    Clean up & add routes externally

--- src/lib/trpc/createClient.ts
+++ src/lib/trpc/createClient.ts
@@ -1,5 +1,5 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
-import type { AppRouter } from '../../pages/api/trpc/[trpc]';
+import type { AppRouter } from './appRouter';
 
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
