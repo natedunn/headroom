2aae198e215f38da9eecbec2d6475de60dbf61ba
Nate Dunn <hello@natedunn.net>
2022-11-03 19:24:14 -0600

    Remove extra comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
 import partytown from '@astrojs/partytown';
-
-// https://astro.build/config
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import prefetch from '@astrojs/prefetch';
 import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 import node from '@astrojs/node';
 
 // https://astro.build/config
